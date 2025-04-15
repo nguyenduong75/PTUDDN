713d07821ffca54448b8565293b1a10d75b8b6e7
nguyenduong75 <131922750+nguyenduong75@users.noreply.github.com>
2025-04-15 22:03:53 +0700

    Add files via upload

---
+++ GKE/GKE/src/main/java/com/example/ProGra/ProGraApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProGra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProGraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProGraApplication.class, args);
+	}
+
+}
---
+++ GKE/GKE/src/test/java/com/example/ProGra/ProGraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProGra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProGraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
